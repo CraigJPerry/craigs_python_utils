9810a29d3334afdc3dad261f190f15b2d40856fc
Craig J Perry <craigp84@gmail.com>
2013-12-11 23:15:53 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""My collection of reusable python utilities."""
+
+
+from setuptools import setup, find_packages
+from os.path import dirname, join
+
+
+__version__ = "0.1.1"
+README = open(join(dirname(__file__), "README.rst")).read()
+REQUIREMENTS = open(join(dirname(__file__), "requirements.txt")).readlines()
+
+
+setup(
+    name="craigs_python_utils",
+    version=__version__,
+    description=__doc__,
+    long_description=README,
+    author="Craig J Perry",
+    author_email="craigp84@gmail.com",
+    install_requires=REQUIREMENTS,
+    package_dir={"": "src"},
+    packages=find_packages("src", exclude=["tests"])
+)
